d3ab46a482de9ce7719449fa058678baa6e34f8e
Sakib1150 <mdshakibmia207@gmail.com>
2025-08-03 13:56:54 +0600

    ERC20ABI.js

---
+++ ERC20ABI.js
@@ -0,0 +1,7 @@
+// ERC20ABI.js
+export const ERC20ABI = [
+  "function balanceOf(address) view returns (uint256)",
+  "function transfer(address to, uint amount) returns (bool)",
+  "function allowance(address owner, address spender) view returns (uint256)",
+  "function approve(address spender, uint256 amount) returns (bool)"
+];
