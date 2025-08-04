822209612abaf7309c9f8e56ddcb108d1f68c103
Sakib1150 <mdshakibmia207@gmail.com>
2025-08-04 20:57:46 +0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
-import React from 'react';
+// src/App.js
+
+import React from "react";
 
 function App() {
   return (
-    <div>
-      <h1>Hello DApp Transfer System!</h1>
+    <div style={{ textAlign: "center", padding: "50px" }}>
+      <h1>DApp Transfer System</h1>
+      <p>This is your deployed React DApp interface.</p>
     </div>
   );
 }
